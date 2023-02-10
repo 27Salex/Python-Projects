c8a03107c165d7f07143c90da0675425cc702cab
a7lm <A7lm27@gmail.com>
2023-02-10 19:42:02 +0100

    RockPaperScissors Finished

--- RockPaperScisors.py
+++ RockPaperScisors.py
@@ -1,13 +1,40 @@
 import random
-def start:
-    Print("Welcome To Rock Paper Scisors!")
-    choice = input("r:Rock, p:Paper or s:Scisors?")
+print("Welcome To Rock Paper Scissors!")
+choice = input("r Rock, p Paper or s Scissors?").lower()
+rivalChoice = random.choice(['r', 'p', 's'])
+if choice == "r" and rivalChoice == 's':
+    print("You Won!")
+elif choice == "r" and rivalChoice == 'p':
+    print("You Lose!")
+elif choice == "r" and rivalChoice == 'r':
+    print("Oops its a Draw")
+elif choice == "p" and rivalChoice == 'r':
+    print("You Won!")
+elif choice == "p" and rivalChoice == 's':
+    print("You Lose!")
+elif choice == "p" and rivalChoice == 'p':
+    print("Oops its a Draw")
+elif choice == "s" and rivalChoice == 'p':
+    print("You Won!")
+elif choice == "s" and rivalChoice == 'r':
+    print("You Lose!")
+elif choice == "s" and rivalChoice == 's':
+    print("Oops its a Draw")
+else:
+    print("Sorry choose between r, p or s")
+
+
+"""
+import random
+def start():
+    print("Welcome To Rock Paper Scisors!")
+    choice = input("r Rock, p Paper or s Scisors?")
     rivalChoice = (random.randrange(1,3)) 
-    """
+   
     1 Rock
     2 Paper
     3 Scisors
-    """
+    
     if choice == "r" and rivalChoice == 3:
         print("You Won!")
     elif choice == "r" and rivalChoice == 2:
@@ -28,3 +55,5 @@ def start:
         print("Oops its a Draw")
     else:
         print("Sorry choose between r, p or s")
+        start()
+"""
