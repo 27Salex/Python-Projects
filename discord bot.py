94c9d2c06f218f8bf7ceb2ee7b2ff30fbfbf2e9e
a7lm <A7lm27@gmail.com>
2023-02-16 00:07:28 +0100

    bot working with both commands

--- discord bot.py
+++ discord bot.py
@@ -1,7 +1,7 @@
 import discord
 
 intents = discord.Intents.all()
-client = discord.Client(command_prefix='!', intents = intents)
+client = discord.Client(command_prefix='/', intents = intents)
 
 @client.event
 async def on_ready():
@@ -9,10 +9,12 @@ async def on_ready():
     
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == client.user: #omprueba si el autor del mensaje del es un usuario 
         return
-    if message.content.startswith("hola"): 
-        await message.channel.send("Buenas amo, puedo ayudarle en algo?")
+    if message.content.startswith("/hola"): #Si el mensaje enviado empieza con la palabra "hola"
+        await message.channel.send("Buenas amo, puedo ayudarle en algo?")  #el bot responde con el mensaje
+    elif message.content.startswith("m-help"): #Si el mensaje enviado empieza con "m-help"
+        await message.channel.send("Here are some commands you can use: m-help,  /dice, /hola, /hug, /play")
         
 client.run("ODY2NzY1MzEyOTU5ODQwMjg2.G876rI.WVJagPYEH5wA2o4kBAktc6D0rw7KuR4io9G-Yg")
 
