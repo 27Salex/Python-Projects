d58f26fc6029fe0f662bcf89ae6e5fe9b646ca42
a7lm <A7lm27@gmail.com>
2023-03-30 09:00:55 +0200

    fixing Password manager

--- PasswordManager.py
+++ PasswordManager.py
@@ -16,22 +16,23 @@ if os.path.isfile('info.txt'):
         website = input("For what is your password? ")
         userName = input("Please enter the user name: ")
         password = input("Please enter the password here: ")
-        encrypted_username = fernet.encrypt(userName.encode())
-        encrypted_password = fernet.encrypt(password.encode())
-        usrnm = "UserName: " + encrypted_username.decode() + "\n"
-        pwd = "Password: " + encrypted_password.decode() + "\n"
-        web = "Website: " + website + "\n"
+        encrypted_username = Fernet.encrypt(userName.encode())
+        encrypted_password = Fernet.encrypt(password.encode())
+        encrypted_web = Fernet.encrypt(website.encode())
+        usrnm = "UserName: " + encrypted_username + "\n"
+        pwd = "Password: " + encrypted_password + "\n"
+        web = "Website: " + encrypted_web + "\n"
         with open("info.txt", 'ab') as file: # utiliza 'ab' para abrir el archivo en modo binario y añadir contenido al final
             file.write("---------------------------------\n".encode())
-            file.write(web.encode())
-            file.write(usrnm.encode())
-            file.write(pwd.encode())
+            file.write(web)
+            file.write(usrnm)
+            file.write(pwd)
             file.write("---------------------------------\n".encode())
             file.write("\n".encode())
     elif choice == 'read':
         with open("info.txt", 'rb') as file: # abre el archivo en modo binario para leer el contenido cifrado
             contenido_cifrado = file.read()
-        contenido_desencriptado = fernet.decrypt(contenido_cifrado).decode() # decodifica el contenido desencriptado
+        contenido_desencriptado = Fernet.decrypt(contenido_cifrado).decode() # decodifica el contenido desencriptado
         print(contenido_desencriptado)
 else:
     print("El archivo 'info.txt' no existe")
